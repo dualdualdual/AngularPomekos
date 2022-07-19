d698cd6e8dc5270d93c7a8c64077493d046d59da
Neremix89 <ngarcial152@gmail.com>
2022-07-19 20:12:58 +0200

    NavBar Routing & Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      NavbarComponent,
+      LoginComponent,
+      PokedexComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
